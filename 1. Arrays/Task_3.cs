54469ea581af1473b0f1435656521244d8a9aeaa
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-10 15:46:29 +0700

    Commit 4
    Load Task 3

---
+++ 1. Arrays/Task_3.cs
@@ -0,0 +1,31 @@
+﻿
+int[][] arr = new int[5][];
+
+for (int i = 0; i < 5; i++)
+{
+    Random rnd = new Random();
+    arr[i] = new int[5];
+
+    for(int y =0; y < 5; y++)
+        arr[i][y] = rnd.Next(1,100);
+       
+}
+
+for(int i=0;i<arr.Length; i++)
+{
+    for(int j=0; j<arr[i].Length; j++)
+    {
+        Console.Write($"{arr[i][j]}\t");
+    }
+    Console.Write("\n");
+}
+int[] arr_max = new int[5];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine($"Сумма элементов строки {i} = {arr[i].Sum()}");
+    Console.WriteLine($"Максимальный элемент в строке {i} = {arr[i].Max()}");
+    arr_max[i] = arr[i].Max();
+}
+
+Console.WriteLine($"Самый большой элемент в массиве {arr_max.Max()}");
