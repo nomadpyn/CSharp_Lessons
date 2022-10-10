6bf2993c341b3beedcf439a6915c6c4a64991692
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-10 15:00:03 +0700

    Commit 2
    Load Task 1

---
+++ 1. Arrays/Task_1.cs
@@ -0,0 +1,58 @@
+﻿
+int[] Array = new int[10];
+
+Console.WriteLine("Выберете как хотите заполнять массив.");
+Console.WriteLine("1 - от 1 до 10\n2 - ввести значения с клавиатуры");
+Console.WriteLine("3 - рандомно\n0 - выйти из программы");
+
+int choise = Int32.Parse(Console.ReadLine());   
+
+switch (choise)
+{
+    case 1:
+        {
+            for (int i = 0; i < 10;)
+                 Array[i] = ++i;
+            break;
+        }
+    case 2:
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Array[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            break;
+        }
+    case 3:
+        {
+            Random rnd = new Random();
+            
+            for(int i = 0; i < 10; i++)
+            {
+                Array[i] = rnd.Next(1,100);
+            }
+            break;
+        }
+    case 0:
+        {
+            Console.WriteLine("До свидания!");
+            break;
+        }
+
+    default:
+        {
+            Console.WriteLine("Вы ничего не выбрали\n");
+            break;
+        }
+
+}
+
+Console.WriteLine("Вывод всего массива на экран");
+
+for (int i = 0; i < 10; i++)
+    Console.Write($"{Array[i]} ");
+
+Console.WriteLine("\nВведите индекс массива от 0 до 9 для вывода элемента");
+choise = Int32.Parse(Console.ReadLine());
+Console.WriteLine($"Индекс {choise} = {Array[choise]} ");
