3d1cf857c5df838fa66c2a37009c3c89095bb4e5
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-12 13:04:15 +0700

    Commit 6
    Load Task_2 to Strings
    Update README.md

---
+++ 2. Strings/Task_2.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Введите слово");
+string? word = Console.ReadLine();
+
+int length = word.Length;
+
+word = word.PadLeft(length*2, '*');
+word = word.PadRight(length*3, '*');
+
+Console.WriteLine(word);
+
+word = word.Replace("*", "#");
+
+Console.WriteLine(word);
+
+word = word.Trim('#');
+
+Console.WriteLine(word);
+
+Console.WriteLine("Введите слово из двух букв для поиска в первом слове");
+string? two = Console.ReadLine();
+
+if (word.Contains(two))
+{
+    Console.WriteLine($"слово \"{two}\" есть в слове \"{word}\" в индексе {word.IndexOf(two)} ");
+}
+else
+    Console.WriteLine($"слово \"{two}\" нет в слове \"{word}\"");
