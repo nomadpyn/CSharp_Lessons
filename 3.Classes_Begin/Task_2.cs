8be58a3cba3b11f5d3d30d8c4b27229ce1c5cbbe
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-14 12:00:57 +0700

    Commit 11
    Load Task_2 to Classes_Begin
    Update README.md

---
+++ 3.Classes_Begin/Task_2.cs
@@ -0,0 +1,82 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Shop Bica = new Shop();
+        Console.WriteLine("Введите название магазина");
+        Bica.setName(Console.ReadLine());
+        Console.WriteLine("Введите адрес магазина");
+        Bica.setLocation(Console.ReadLine());
+        Console.WriteLine("Введите описание магазина");
+        Bica.setData(Console.ReadLine());
+        Console.WriteLine("Введите телефон магазина");
+        Bica.setPhone(Console.ReadLine());
+        Console.WriteLine("Введите Email магазина");
+        Bica.setEmail(Console.ReadLine());
+
+        Bica.show();
+
+        Console.ReadKey();
+
+
+    }
+}
+
+class Shop
+{
+    private string? name;
+    private string? loc;
+    private string? data;
+    private string? phone;
+    private string? email;
+
+    public void setName(string nameP)
+    {
+        this.name = nameP;  
+    }
+    public void setLocation(string locP)
+    {
+        this.loc = locP;
+    }
+    public void setData(string dataP)
+    {
+        this.data = dataP;
+    }
+    public void setPhone(string phoneP)
+    {
+        this.phone = phoneP;
+    }
+    public void setEmail(string emailP)
+    {
+        this.email = emailP;
+    }
+    public string getName()
+    {
+        return this.name;
+    }
+    public string getLoc()
+    {
+        return this.loc;
+    }
+    public string getData()
+    {
+        return this.data;
+    }
+    public string getPhone()
+    {
+        return this.phone;
+    }
+    public string getEmail()
+    {
+        return this.email;
+    }
+    public void show()
+    {
+        Console.WriteLine($"Названия магазина: {this.getName()}");
+        Console.WriteLine($"Адрес магазина: {this.getLoc()}");
+        Console.WriteLine($"Описание магазина: {this.getData()}");
+        Console.WriteLine($"Телефон: {this.getPhone()} / Email: {this.getEmail()}");
+    }
+}
