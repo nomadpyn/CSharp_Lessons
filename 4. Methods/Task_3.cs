8ffe57ea4fd2e43bdb930e26aa90ac848115cb00
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-19 12:04:24 +0700

    Commit 18
    Load Task_3 to Methods
    Update README.md
    Try add README to folders

---
+++ 4. Methods/Task_3.cs
@@ -0,0 +1,48 @@
+﻿
+Employer A = new Employer();
+
+Console.WriteLine("Введите имя сотрудника");
+A.Name = Console.ReadLine();
+Console.WriteLine("Введите возраст сотрудника");
+A.Age = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите должность сотрудника");
+A.Work = Console.ReadLine();
+Console.WriteLine("Введите зарплату сотрудника");
+A.Pay = int.Parse(Console.ReadLine());
+
+A.print();
+
+class Employer
+{
+	private string name;
+	private string work;
+	private int age;
+	private int pay;
+	public string Name
+	{
+		get { return this.name != null ? this.name : "Нет данных"; }
+		set { this.name = value; }
+	}
+	public string Work
+	{
+		get { return this.work != null ? this.work : "Нет данных"; }
+		set { this.work = value; }
+    }
+	public int Age
+	{
+        get { return this.age; }
+        set { this.age = (value < 1 || value > 100) ? 0 : value; }
+    }
+	public int Pay
+	{
+		get { return this.pay; }
+		set { this.pay = value < 0 ? 0 : value;}
+	}
+	public void print()
+	{
+		Console.WriteLine($"Имя: {this.name}");
+        Console.WriteLine($"Возраст: {this.age}");
+        Console.WriteLine($"Должность: {this.work}");
+        Console.WriteLine($"Зарплата: {this.pay}");
+    }
+}
