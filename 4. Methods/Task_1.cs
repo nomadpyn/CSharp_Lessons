b1ad4eac2913bd44d0320d375828c2d6a9ba75ce
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-19 11:23:40 +0700

    Commit 16
    Load Task_1 to Methods

---
+++ 4. Methods/Task_1.cs
@@ -0,0 +1,32 @@
+﻿
+int i;
+getSize(out i);
+
+string[] arr= new string[1];
+
+getArr(ref arr, ref i);
+showArr(ref arr);
+
+static void getSize(out int digit)
+{
+    Console.WriteLine("Введите размер массива");
+    digit = Int32.Parse(Console.ReadLine());
+}
+
+static void getArr(ref string[] arr, ref int size)
+{
+    Array.Clear(arr);
+    Array.Resize(ref arr, size);
+    Console.WriteLine("Заполните массив с клавиатуры словами");
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = Console.ReadLine();
+}
+
+static void showArr(ref string[] arr)
+{
+    for(int i=0; i<arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
