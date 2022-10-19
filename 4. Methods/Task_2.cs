49fb790100b2e05537ed2902f01bdfe9c8f97ceb
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-19 11:37:39 +0700

    Commit 17
    Load Task_2 to Methods

---
+++ 4. Methods/Task_2.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Самое большое слово " + maxWord("привет","красивый","мир"));
+
+Console.WriteLine("Самое маленькое слово " + minWord("привет", "красивый", "мир"));
+
+static string maxWord(params string[] words)
+{
+    string temp = words[0];
+
+    for (int i = 1; i < words.Length; i++)
+    {
+        if (words[i].Length > temp.Length)
+            temp = words[i];
+    }
+
+    return temp;
+
+}
+
+static string minWord(params string[] words)
+{
+    string temp = words[0];
+
+    for (int i = 1; i < words.Length; i++)
+    {
+        if (words[i].Length < temp.Length)
+            temp = words[i];
+    }
+
+    return temp;
+}
