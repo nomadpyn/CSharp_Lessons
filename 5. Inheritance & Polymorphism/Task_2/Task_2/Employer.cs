c0dd0093c0a05d8f07504e7b8b2b9dd54ab770dd
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-26 10:13:44 +0700

    Commit 25
    Task_2 to Inheritance & Polymorphism
    Create class Employer

---
+++ 5. Inheritance & Polymorphism/Task_2/Task_2/Employer.cs
@@ -0,0 +1,16 @@
+﻿internal abstract class Employer : Human
+{
+    protected string? work_location;
+    protected int pay;
+    public Employer(string nameP, string fnameP, int age, string work_locationP, int payP)
+    : base(nameP, fnameP, age)
+    {
+        this.work_location = work_locationP;
+        this.pay = payP;
+    }
+    protected virtual new void Print()
+    {
+        base.Print();
+        Console.WriteLine($"Зарплата: {this.pay}, Адрес работы: {this.work_location}");
+    }
+}
