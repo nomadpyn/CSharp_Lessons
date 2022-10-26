547e80af60edf210fc821ee9635743e2f081e823
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-26 10:22:28 +0700

    Commit 26
    Task_2 to Inheritance & Polymorphism
    Create sealed class Headmaster

---
+++ 5. Inheritance & Polymorphism/Task_2/Task_2/Headmaster.cs
@@ -0,0 +1,15 @@
+﻿internal sealed class Headmaster : Human
+{
+    protected string? office_location;
+    public Headmaster(string nameP, string fnameP, int age, string office_locationP)
+    : base(nameP, fnameP, age)
+    {
+        this.office_location = office_locationP;
+       
+    }
+    public new void Print()
+    {
+        base.Print();
+        Console.WriteLine($"Адрес кабинета: {this.office_location}");
+    }
+}
