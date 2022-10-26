58a66d3df7ab647ef7b678eb98275a2d935d818c
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-26 11:06:25 +0700

    Commit 30
    Task_2 to Inheritance & Polymorphism
    Create class Driver

---
+++ 5. Inheritance & Polymorphism/Task_2/Task_2/Driver.cs
@@ -0,0 +1,17 @@
+﻿internal class Driver : Employer
+{
+    string? transport;
+    string? number;
+    public Driver(string nameP, string fnameP, int ageP, string work_locationP,
+        int payP, string transportP, string numberP)
+        : base(nameP, fnameP, ageP, work_locationP, payP)
+    {
+        this.transport = transportP;
+        this.number = numberP;
+    }
+    public virtual new void Print()
+    {
+        base.Print();
+        Console.WriteLine($"Транспорт: {this.transport} - {this.number}");
+    }
+}
