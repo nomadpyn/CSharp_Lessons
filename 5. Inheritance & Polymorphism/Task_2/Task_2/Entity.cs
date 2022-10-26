3531f49b60083bd3a1fa135831257a5a571490c7
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-26 10:34:41 +0700

    Commit 27
    Task_2 to Inheritance & Polymorphism
    Create class Entity
    Update REAMDE.md

---
+++ 5. Inheritance & Polymorphism/Task_2/Task_2/Entity.cs
@@ -0,0 +1,14 @@
+﻿internal class Entity : Buyer
+{
+    string? company;
+    public Entity(string nameP, string fnameP, int age, string payment_typeP, string companyP)
+        : base(nameP, fnameP, age, payment_typeP)
+    {
+        this.company = companyP;
+    }
+    public virtual new void Print()
+    {
+        base.Print();
+        Console.WriteLine($"Место доставки: {this.company}");
+    }
+}
