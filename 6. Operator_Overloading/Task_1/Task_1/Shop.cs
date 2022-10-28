161a2c0216822448e9bf664cf1009c44bab62e19
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-28 11:13:10 +0700

    Commit 32
    Add Task_11 to Operator_Overloading
    Create Class Shop
    Add Constructor and ToStrint()

---
+++ 6. Operator_Overloading/Task_1/Task_1/Shop.cs
@@ -0,0 +1,17 @@
+﻿ internal class Shop
+    {
+    string? name;
+    string? adress;
+    double square;
+
+    public Shop(string nameP,string adressP, double squareP)
+    {
+        this.name = nameP;
+        this.adress = adressP;
+        this.square = squareP;
+    }
+    public override string ToString()
+    {
+        return $"Магазин \"{this.name}\" по адресу {this.adress}, площадью {this.square} кв.м.";
+    }
+}
