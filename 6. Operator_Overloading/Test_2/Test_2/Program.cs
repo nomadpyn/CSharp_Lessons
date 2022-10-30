f5f697e95842156f9983b45d908d24f912de8f83
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-30 14:21:52 +0700

    Commit 38
    Operator_Overloading Task_1 :
    - fix in class from double to int
    - create static method Parse

--- 6. Operator_Overloading/Test_2/Test_2/Program.cs
+++ 6. Operator_Overloading/Test_2/Test_2/Program.cs
@@ -1,1 +1,5 @@
 ﻿
+Quadratic_Equation nw = Quadratic_Equation.Parse(Console.ReadLine());
+
+Console.WriteLine(nw.A);
+Console.WriteLine(nw.B);
--- 6. Operator_Overloading/Test_2/Test_2/Quadratic Equation.cs
+++ 6. Operator_Overloading/Test_2/Test_2/Quadratic Equation.cs
@@ -1,16 +1,30 @@
 ﻿internal class Quadratic_Equation
     {
-    protected double a=0;
-    protected double b=0;
-    public double A
+    protected int a=0;
+    protected int b=0;
+    public int A
     {
         get { return a; }
         set { this.a = value; }
     }
-    public double B
+    public int B
     {
         get { return b; }
         set { this.b = value; }
     }
+    public static Quadratic_Equation Parse(string obj)
+    {
+        Quadratic_Equation equ = new Quadratic_Equation();
+        string[] arr = obj.Split('x', '+', '=');
+        equ.A = Convert.ToInt32(arr[0]);
+        if (arr[1] != "")
+        {
+            equ.B = Convert.ToInt32(arr[1]);
+        }
+        else
+            equ.B = Convert.ToInt32(arr[2]);
+
+        return equ;
+    } 
     }
 
