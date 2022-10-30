d5e9612452fe2f6c5b953dc9603f3811023ac347
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-30 14:03:07 +0700

    Commit 37
    Operator_Overloading Task_1 :
    - create Class Quadratic_Equation
    - add get and set

---
+++ 6. Operator_Overloading/Test_2/Test_2/Quadratic Equation.cs
@@ -0,0 +1,16 @@
+﻿internal class Quadratic_Equation
+    {
+    protected double a=0;
+    protected double b=0;
+    public double A
+    {
+        get { return a; }
+        set { this.a = value; }
+    }
+    public double B
+    {
+        get { return b; }
+        set { this.b = value; }
+    }
+    }
+
