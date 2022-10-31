8983c7bd6588616330cdb1ad3d02f070704a0e4c
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-31 13:43:52 +0700

    Commit 44
    Operator_Overloading Task_3:
    - Create Main

---
+++ 6. Operator_Overloading/Task_3/Task_3/Program.cs
@@ -0,0 +1,48 @@
+﻿
+Book_List A = new Book_List();
+
+int choise;
+do
+{
+    Console.WriteLine("Список книг");
+    Console.WriteLine("1- вывести все книги, 2 - добавить книгу, 3 - удалить книгу, 4 - прочитать книгу, 0 - выход");
+    choise = int.Parse(Console.ReadLine());
+    switch (choise)
+    {
+        case 1:
+            {
+                Console.WriteLine(A);
+                A.showBooks();
+                break;
+            }
+        case 2:
+            {
+                Console.WriteLine(A);
+                A.addBook();
+                break;
+            }
+        case 3:
+            {
+                Console.WriteLine(A + "\nКакую удалить?");
+                A.removeBook();
+                break;
+            }
+        case 4:
+            {
+                Console.WriteLine(A+ "\nКакую прочесть?");
+                A.readBook();
+                break;
+            }
+        case 0:
+            {
+                Console.WriteLine("До свидания!");
+                break;
+            }
+        default:
+            {
+                Console.WriteLine("Ничего не выбрали");
+                break;
+            }
+    }
+}
+while (choise != 0);
