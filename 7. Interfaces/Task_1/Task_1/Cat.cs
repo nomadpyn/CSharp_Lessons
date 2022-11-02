f6d0594476281578f9a8d5dc88b754ff1ad946f5
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-02 11:49:18 +0700

    Commit 50
    Interfaces Task_1:
    - create class Dog
    - create class Cat

---
+++ 7. Interfaces/Task_1/Task_1/Cat.cs
@@ -0,0 +1,38 @@
+﻿internal class Cat : Pet, ICompPet, ICompanion
+{
+    bool isCompanion = true;
+    string? description;
+    string? master;
+    public Cat(string nameP, uint ageP, string descP, string masterP) : base(nameP, ageP)
+    {
+        this.description = descP;
+        this.master = masterP;
+    }
+    public bool IsCompanion
+    {
+        get { return isCompanion; }
+    }
+    public string Description
+    {
+        get { return this.description; }
+        set { this.description = value; }
+    }
+    public string Master
+    {
+        get { return this.master; }
+        set { this.master = value; }
+    }
+    public string TypeName()
+    {
+        return "Кошка";
+    }
+    public string Voice()
+    {
+        return "Мяу-Мяу";
+    }
+    public override string ToString()
+    {
+        string add = $"\nХозяин {this.master}: {this.description}";
+        return $"{this.TypeName()} {base.ToString()} говорит {this.Voice()} {add}";
+    }
+}
---
+++ 7. Interfaces/Task_1/Task_1/Dog.cs
@@ -0,0 +1,38 @@
+﻿internal class Dog : Pet, ICompPet, ICompanion
+{
+    bool isCompanion = true;
+    string? description;
+    string? master;
+    public Dog(string nameP, uint ageP, string descP,string masterP) : base(nameP, ageP)
+    {
+        this.description = descP;
+        this.master = masterP;
+    }
+    public bool IsCompanion
+    {
+        get { return isCompanion; }
+    }
+    public string Description
+    {
+        get { return this.description; }
+        set { this.description = value; }
+    } 
+    public string Master
+    {
+        get { return this.master; }
+        set { this.master = value; }
+    }
+    public string TypeName()
+    {
+        return "Собака";
+    }
+    public string Voice()
+    {
+        return "Гав-Гав";
+    }
+    public override string ToString()
+    {
+        string add = $"\nХозяин {this.master}: {this.description}";
+        return $"{this.TypeName()} {base.ToString()} говорит {this.Voice()} {add}";
+    }
+}
