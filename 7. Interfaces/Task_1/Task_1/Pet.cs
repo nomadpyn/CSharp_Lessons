5a8818baa5f68df7225aa2f452ed23e354fcaedf
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-02 11:02:58 +0700

    Commit 45
    Interfaces Task_1:
    - create class Pet

---
+++ 7. Interfaces/Task_1/Task_1/Pet.cs
@@ -0,0 +1,15 @@
+﻿internal abstract class Pet
+{
+    protected string? name;
+    protected uint age;
+    public Pet(string nameP, uint ageP)
+    {
+        this.name = nameP;
+        this.age = ageP;
+    }
+    public override string ToString()
+    {
+        return $"{this.name}, возрастом {this.age}";
+    }
+
+}
