1ffdc91e59a59278a75629e22a7c3e939e12b44c
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-02 12:00:28 +0700

    Commit 51
    Ended Interfaces Task_1

--- 7. Interfaces/Task_1/Task_1/Program.cs
+++ 7. Interfaces/Task_1/Task_1/Program.cs
@@ -1,1 +1,16 @@
 ﻿
+ICompPet [] House = new ICompPet[4];
+
+House[0] = new Cat("Мурка", 4, "Рыжая кожка", "Маша");
+House[1] = new Dog("Барбос", 6, "Дворняга", "Дед Максим");
+House[2] = new Cow("Буренка", 5, "Хлев");
+House[3] = new Chicken("Клава", 1, "Курятник");
+
+foreach (ICompPet item in House)
+{
+    if (item.IsCompanion)
+    {
+        Console.WriteLine(item);
+        Console.WriteLine();
+    }
+}
