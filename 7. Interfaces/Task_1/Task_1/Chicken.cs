358aa0477b1af7c202df67c31064a95e6dd2ab6e
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-02 11:38:58 +0700

    Commit 49
    Interfaces Task_1:
    - create class Chicken

---
+++ 7. Interfaces/Task_1/Task_1/Chicken.cs
@@ -0,0 +1,36 @@
+﻿internal class Chicken : Pet, ICompPet, IWorker
+{
+    bool isCompanion = false;
+    string? location;
+    string result = "Яйцо";
+    public Chicken(string nameP, uint ageP, string locP) : base(nameP, ageP)
+    {
+        this.location = locP;
+    }
+    public bool IsCompanion
+    {
+        get { return this.isCompanion; }
+    }
+    public string Location
+    {
+        get { return this.location; }
+        set { this.location = value; }
+    }
+    public string Result
+    {
+        get { return this.result; }
+    }
+    public string TypeName()
+    {
+        return "Курица";
+    }
+    public string Voice()
+    {
+        return "Ко-Ко-Ко";
+    }
+    public override string ToString()
+    {
+        string add = $"\nЖивет в {this.location} и дает {this.result}";
+        return $"{this.TypeName()} {base.ToString()} говорит {this.Voice()} {add}";
+    }
+}
