c94a5e10def04da61234083a04548d60743283e4
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-07 13:39:18 +0700

    Commit 61
    Exceptions add Task_1

---
+++ 8. Exceptions/Task_1/Task_1/MyExcept.cs
@@ -0,0 +1,11 @@
+﻿[Serializable]
+public class MyExcept : Exception
+{
+	public DateTime TimeExcept { get; private set; }
+	public MyExcept() : this("Ошибка") { TimeExcept = DateTime.Now; }
+	public MyExcept(string message) : base(message) { TimeExcept = DateTime.Now; }
+	public MyExcept(string message, Exception inner) : base(message, inner) { }
+	protected MyExcept(
+	  System.Runtime.Serialization.SerializationInfo info,
+	  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
---
+++ 8. Exceptions/Task_1/Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+
+try
+{
+    if (A % B != 0)
+        throw new MyExcept("Не делится без остатка!");
+    else
+        Console.WriteLine("A делится на B без остатка");
+}
+catch(MyExcept My)
+{
+    Console.WriteLine(My.Message);
+    Console.WriteLine(My.TimeExcept);
+}
+finally
+{
+    Console.WriteLine("Очистка ресурсов");
+}
