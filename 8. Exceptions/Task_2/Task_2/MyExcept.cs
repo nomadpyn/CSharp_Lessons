37b0d279fd10ac0dffca9f3907474a0ee423515a
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-07 14:01:47 +0700

    Commit 62
    Exceptions add Task_2

---
+++ 8. Exceptions/Task_2/Task_2/MyExcept.cs
@@ -0,0 +1,8 @@
+﻿[Serializable]
+public class MyExcept : Exception
+{
+	public DateTime TimeExcept;
+	public MyExcept() : this("Ошибка") { TimeExcept = DateTime.Now; }
+	public MyExcept(string message) : base(message) { TimeExcept = DateTime.Now; }
+	
+}
---
+++ 8. Exceptions/Task_2/Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[] arr = new int[10];
+
+for(int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 15);
+}
+
+try
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        try
+        {
+            if (arr[i] % 2 == 0)
+                throw new MyExcept("Число четное");
+            Console.WriteLine($"Индекс {i} - число {arr[i]}");
+        }
+        catch (MyExcept ex)
+        {
+            Console.WriteLine($"Индекс {i} - число {arr[i]} {ex.Message} Время {ex.TimeExcept}");
+        }
+    }
+}
+catch(IndexOutOfRangeException ex)
+{
+    Console.WriteLine("Выход за границы массива");
+    Console.WriteLine(ex.Message);
+}
