6740f6fe1f3ee2f0cecd44017248b48a7283bf99
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-18 13:04:38 +0700

    Commit 64
    Update README
    Delegate Add Task_2

---
+++ 9. Delegate/Task_2/Task_2/Data.cs
@@ -0,0 +1,43 @@
+﻿internal class Data
+{
+    public void dataInt(int a)
+    {
+        for(int i=0;i< sizeof(int); i++)
+        {
+            Console.Write(a + "\t");
+        }
+        Console.WriteLine();
+    }
+    public void dataDouble(double a)
+    {
+        for (int i = 0; i < sizeof(double); i++)
+        {
+            Console.Write("{0:F3}\t",a);
+        }
+        Console.WriteLine();
+    }
+    public void dataDecimal(decimal a)
+    {
+        for (int i = 0; i < sizeof(decimal); i++)
+        {
+            Console.Write("{0:F3}\t", a);
+        }
+        Console.WriteLine();
+    }
+    public void dataChar(char a)
+    {
+        for (int i = 0; i < sizeof(char); i++)
+        {
+            Console.Write(a + "\t");
+        }
+        Console.WriteLine();
+    }
+    public void dataString(string a)
+    {
+        for (int i = 0; i < System.Text.ASCIIEncoding.Unicode.GetByteCount(a); i++)
+        {
+            Console.Write(a + "\t");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 9. Delegate/Task_2/Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Data A = new Data();
+
+showData<int> del1 = A.dataInt;
+del1(34);
+showData<double> del2 = A.dataDouble;
+del2(3.25345345);
+showData<decimal> del3 = A.dataDecimal;
+del3((decimal)17.4234);
+showData<char> del4 = A.dataChar;
+del4('f');
+showData<string> del5 = A.dataString;
+del5("hello");
+
+
+delegate void showData<T>(T a);
