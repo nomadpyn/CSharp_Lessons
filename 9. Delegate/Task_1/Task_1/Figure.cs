1ac476cb65221e0f920ebcc10345acb073a7cd9c
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-18 12:42:34 +0700

    Commit 64
    Update README
    Delegate Add Task_1

---
+++ 9. Delegate/Task_1/Task_1/Figure.cs
@@ -0,0 +1,23 @@
+﻿using System.Transactions;
+
+internal class Figure
+    {
+    public double X { get; set; }
+    public Figure(double x)
+    {
+        this.X = x;
+    }
+    public double sqTriangle()
+    {
+        double h = Math.Pow(this.X, 2) - Math.Pow((this.X / 2), 2);
+        return (this.X * (h / 2));
+    }
+    public double sqQuad()
+    {
+        return Math.Pow(this.X, 2);
+    }
+    public double sqCircle()
+    {
+        return (Math.PI * Math.Pow(this.X, 2));
+    }
+    }
---
+++ 9. Delegate/Task_1/Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Figure A = new Figure(3.2);
+
+figSquare del = A.sqTriangle;
+del += A.sqQuad;
+del += A.sqCircle;
+
+int i = 0;
+
+foreach(figSquare item in del.GetInvocationList())
+{
+    Console.WriteLine("Squre of {0} is {1:F}", (Fig)i, item());
+    i++;
+}
+enum Fig
+{
+    Triangle,
+    Quad,
+    Circle
+};
+
+delegate double figSquare();
