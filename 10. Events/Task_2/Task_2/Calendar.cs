ee5bbdb3545caf331db39f759223c31508d1b120
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-21 12:38:23 +0700

    Commit 70
    Events add Task_2

---
+++ 10. Events/Task_2/Task_2/Calendar.cs
@@ -0,0 +1,15 @@
+﻿    internal class Calendar
+    {
+    public delegate void Del(Months now);
+    public event Del? itsTime;
+    public void monthCount()
+    {
+        for(Months i = Months.Январь; i<= Months.Декабрь; i++)
+        {
+            if ((byte)i % 3 == 0)
+            {
+                itsTime(i);
+            }
+        }
+    }
+    }
---
+++ 10. Events/Task_2/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Calendar nw = new Calendar();
+
+nw.itsTime += Message;
+
+nw.monthCount();
+
+void Message(Months now)
+{
+    Console.WriteLine($"{now} начался. Сезон сменился,пора менять одежду и обувь");
+}
+enum Months : byte 
+{ 
+    Январь=1,
+    Февраль,
+    Март,
+    Апрель,
+    Май,
+    Июнь,
+    Июль,
+    Август,
+    Сентябрь,
+    Октябрь,
+    Ноябрь,
+    Декабрь
+}
+
