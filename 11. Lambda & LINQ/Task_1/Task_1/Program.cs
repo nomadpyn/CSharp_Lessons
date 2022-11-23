27b38301504a29c79adfd0909df1442fadfa011d
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-23 13:41:22 +0700

    Commit 74
    Update README
    Lambda&LINQ add Task_1

---
+++ 11. Lambda & LINQ/Task_1/Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+show Message = () => Console.WriteLine("Будем складывать,вычитать и умножать Int");
+
+Message();
+
+doSomething Move = (int x, int y) => Console.WriteLine($"Сумма {x + y}");
+Move+= (int x, int y) => Console.WriteLine($"Разность {x - y}");
+Move += (int x, int y) => Console.WriteLine($"Произведение {x * y}");
+
+Move(3, 5);
+
+delegate void show();
+delegate void doSomething(int x,int y);
