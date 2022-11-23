eede8614538af180cf692c4c5b7a0e088de83bdb
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-23 15:19:34 +0700

    Commit 79
    Lambda&LINQ add Task_4
    create Programm

--- 11. Lambda & LINQ/Task_4/Task_4/Program.cs
+++ 11. Lambda & LINQ/Task_4/Task_4/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using Humanity;
+
+List<Human> people = new List<Human>
+{
+    new Human("Петров", "Иван",25),
+    new Human("Сидорова", "Маша",21),
+    new Human("Будро", "Самсон",35),
+    new Human("Иванова", "Елена",45),
+    new Human("Петрович", "Тихон",60),
+    new Human("Топлева", "Ксения",18)
+};
+
+
+Console.WriteLine("Введите возраст, с которого отсортировать людей");
+int sort = int.Parse(Console.ReadLine());
+
+List<Human> selectedPeople = people.Where(h => h.Age >= sort).OrderBy(h => h.Fname).ToList();
+
+foreach (Human i in selectedPeople)
+{
+    Console.WriteLine(i);
+}
