01cb5e45cbd40b02a30b6781ba13e6b92ac6bc9c
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-23 15:19:06 +0700

    Commit 78
    Lambda&LINQ add Task_4
    create class Human

---
+++ 11. Lambda & LINQ/Task_4/Task_4/Human.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humanity
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Fname { get;set; }
+        public int Age { get; set; }
+        public Human(string f, string n,int a)
+        {
+            this.Name = n;
+            this.Fname = f;
+            this.Age = a;
+        }
+        public override string ToString()
+        {
+            return $"{this.Fname} {this.Name}. Возраст {this.Age}";
+        }
+    }
+}
