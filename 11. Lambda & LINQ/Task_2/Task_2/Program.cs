75285031a3386a7612070b690bd41e323a6bfecb
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-23 14:54:48 +0700

    Commit 76
    Lambda&LINQ add Task_2
    Update README

---
+++ 11. Lambda & LINQ/Task_2/Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int[] arr = new int[10];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 100);
+}
+
+var ThreeNotFive = from i in arr where (i % 5 != 0 && i % 3 == 0) select i;
+
+foreach(int i in ThreeNotFive)
+{
+    Console.Write(i +"\t");
+}
