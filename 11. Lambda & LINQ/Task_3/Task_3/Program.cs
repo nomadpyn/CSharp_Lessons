5b016e0f79c699d2cb1b4397a0ef2f9f30cc8b3d
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-23 15:00:13 +0700

    Commit 77
    Lambda&LINQ add Task_3

---
+++ 11. Lambda & LINQ/Task_3/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using System.Collections;
+
+string[] arr = new string[10];
+
+for (int i =0;i<arr.Length; i++)
+{
+    Console.WriteLine($"Введите слово {i+1} из {arr.Length}");
+    arr[i] = Console.ReadLine();
+}
+
+IEnumerable<IGrouping<char, string>> query = from i in arr orderby i ascending group i by i[0] ;
+
+foreach (IGrouping<char, string> key in query)
+{
+    Console.Write($"Key: {key.Key}\nValue:");
+    foreach (string item in key)
+    {
+        Console.Write($"\t{item}");
+    }
+    Console.WriteLine();
+}
