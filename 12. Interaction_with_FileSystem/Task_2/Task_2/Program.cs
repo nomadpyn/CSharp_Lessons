71e1fcf637ab27f7fa778d223d6d0b281fe21252
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-24 16:36:41 +0700

    Commit 83
    Files add Task_2

---
+++ 12. Interaction_with_FileSystem/Task_2/Task_2/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Text;
+
+string path = "int_test.dat";
+
+int[] arr = {5, 7, 8, 34, 1, 54, 12, 36, 78, 12 };
+
+SaveToFile(path, arr);
+int[] arr2 = new int[7];
+FromFileToArr(path, arr2);
+
+foreach(int i in arr2)
+{
+    Console.Write($"{i} ");
+}
+
+void SaveToFile(string path, int[] arr)
+{
+    using(FileStream fs = new FileStream(path, FileMode.Create))
+    {
+        using(BinaryWriter bw = new BinaryWriter(fs, Encoding.Unicode))
+        {
+            Console.WriteLine($"Сохранение данных массива в файл {path}");
+            foreach(int i in arr)
+            {
+                bw.Write(i);
+            }
+        }
+    }
+    Console.WriteLine("Данные успешно записаны");
+}
+
+void FromFileToArr(string path, int[] arr)
+{
+    Console.WriteLine($"Считываение файлов в массив длиной {arr.Length}");
+    using(FileStream fs = new FileStream(path, FileMode.Open))
+    {
+        using (BinaryReader bw = new BinaryReader(fs, Encoding.Unicode))
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = bw.ReadInt32();
+            }
+        }
+    }
+    Console.WriteLine("Считывание прошло успешно");
+}
