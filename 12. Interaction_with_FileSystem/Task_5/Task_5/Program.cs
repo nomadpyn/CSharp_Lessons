15da289a2021ff7acf36d499f9cc66bdd4de1d03
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-28 12:06:09 +0700

    Commit 88
    Files add Task_5
    Update README

---
+++ 12. Interaction_with_FileSystem/Task_5/Task_5/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text.RegularExpressions;
+
+string skorogovor = "Расскажите про покупки\nПро какие про покупки?\nПро покупки, про покупки,\nПро покупочки мои..\n";
+
+Console.WriteLine(skorogovor);
+
+checkBuy(skorogovor);
+
+Console.WriteLine(remakeString(skorogovor));
+
+static void checkBuy(string data)
+{
+    Regex regex = new Regex(@"\w*окуп\w*");
+
+    var matches = regex.Matches(data);
+
+    Console.WriteLine($"Про покупки упоминается {matches.Count} раз");
+    foreach (Match match in matches)
+        Console.WriteLine(match.Value);
+}
+
+static string remakeString(string data)
+{
+    Console.WriteLine("На что поменять покупки?");
+    string replace = Console.ReadLine();
+    Regex regex = new Regex(@"\w*окуп\w*");
+
+    return regex.Replace(data, replace);
+
+}
