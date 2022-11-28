fb6170652d6d5cdffd1133fdb5ed7d15288e02c8
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-28 11:40:39 +0700

    Commit 87
    Files add Task_4
    Update README

---
+++ 12. Interaction_with_FileSystem/Task_4/Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Введите номер телефона");
+
+checkPhoneNumber(Console.ReadLine());
+
+
+static void checkPhoneNumber(string data)
+{
+    Regex regex = new Regex(@"(8){1}-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}");
+
+    if (regex.IsMatch(data))
+    {
+        Console.WriteLine("Номер записан правильно");
+    }
+    else
+    {
+        Console.WriteLine("Ошибка");
+    }
+}
