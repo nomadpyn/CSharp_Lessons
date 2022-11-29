7db33251a8375b239f83d82e87b9406273578e50
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-29 14:24:05 +0700

    Commit 92
    Serializable add Task_1:
    class PayBill realise ISerializable

--- 13. Serializable & XML/Task_1/Task_1/PayBill.cs
+++ 13. Serializable & XML/Task_1/Task_1/PayBill.cs
@@ -1,8 +1,10 @@
-﻿using System.Transactions;
+﻿using System.Runtime.Serialization;
+using System.Transactions;
 
 [Serializable]
-public class PayBill
+public class PayBill: ISerializable
 {
+    public static bool format = false;
     public double Pay { get; set; }
     public byte Days { get; set; }
     public double FineForDay { get; set; }
@@ -10,7 +12,27 @@ public class PayBill
     public double Sum { get; set; }
     public double Fine { get; set; }
     public double BillToPay{ get; set; }
-
+    public PayBill() { }
+    private PayBill(SerializationInfo info, StreamingContext context)
+    {
+        if (PayBill.format)
+        {
+            this.Pay = info.GetDouble("Pay");
+            this.Days = info.GetByte("Days");
+            this.FineForDay = info.GetDouble("FineForDay");
+            this.FineDays = info.GetByte("FineDays");
+            this.Sum = info.GetDouble("Sum");
+            this.Fine = info.GetDouble("Fine");
+            this.BillToPay = info.GetDouble("BillToPay");
+        }
+        else
+        {
+            this.Pay = info.GetDouble("Pay");
+            this.Days = info.GetByte("Days");
+            this.FineForDay = info.GetDouble("FineForDay");
+            this.FineDays = info.GetByte("FineDays");
+        }
+    }
     public PayBill(double pay,byte days,double fineForDay, byte fineDays)
     {
         this.Pay = pay;
@@ -27,10 +49,29 @@ public class PayBill
     public override string ToString()
     {
         if (this.BillToPay != 0)
-            return $"Сумма за услуги {this.Sum} руб\nШтраф {this.Fine} руб\nСумма к оплате {this.BillToPay} руб";
+            return $"Сумма за услуги {this.Sum:F} руб\nШтраф {this.Fine:F} руб\nСумма к оплате {this.BillToPay:F} руб";
         else
             return "Счет на оплату не сформирован.";
     }
-
-
+    void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        if (PayBill.format)
+        {
+            info.AddValue("Pay", this.Pay);
+            info.AddValue("Days", this.Days);
+            info.AddValue("FineForDay", this.FineForDay);
+            info.AddValue("FineDays", this.FineDays);
+            info.AddValue("Sum", this.Sum);
+            info.AddValue("Fine", this.Fine);
+            info.AddValue("BillToPay", this.BillToPay);
+        }
+        else
+        {
+            info.AddValue("Pay", this.Pay);
+            info.AddValue("Days", this.Days);
+            info.AddValue("FineForDay", this.FineForDay);
+            info.AddValue("FineDays", this.FineDays);
+            
+        }
+    }
 }
