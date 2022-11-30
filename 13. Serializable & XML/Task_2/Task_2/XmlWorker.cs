6232b90e68df044915331f0af7ccdb02645e7dae
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-30 11:43:59 +0700

    Commit 95
    update README
    Serializable add Task_2:
    create class XmlWorker

---
+++ 13. Serializable & XML/Task_2/Task_2/XmlWorker.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml;
+internal class XmlWorker
+{
+
+public static void createXml()
+    {
+        XmlTextWriter writer = null;
+
+        try
+        {
+            writer = new XmlTextWriter("Order.xml", System.Text.Encoding.Unicode);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Order");
+            writer.WriteStartElement("Order_Element");
+            writer.WriteElementString("Name", "Молоко");
+            writer.WriteElementString("Price", "65.4");
+            writer.WriteElementString("Amount", "3");
+            writer.WriteEndElement();
+            writer.WriteStartElement("Order_Element");
+            writer.WriteElementString("Name", "Хлеб");
+            writer.WriteElementString("Price", "21.3");
+            writer.WriteElementString("Amount", "1");
+            writer.WriteEndElement();
+            writer.WriteStartElement("Order_Element");
+            writer.WriteElementString("Name", "Мясо");
+            writer.WriteElementString("Price", "357.12");
+            writer.WriteElementString("Amount", "3");
+            writer.WriteEndElement();
+            writer.WriteEndElement();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            if (writer != null)
+                writer.Close();
+        }
+    }
+}  
